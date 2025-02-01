b74ac04b7c46512fa2ab05f157d75ad7e88fbe85
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-01 17:06:45 +0800

    launching the app when notification is clicked

--- app/src/main/java/com/ralphmarondev/cowntdown/core/worker/FirstDayOfMonthWorker.kt
+++ app/src/main/java/com/ralphmarondev/cowntdown/core/worker/FirstDayOfMonthWorker.kt
@@ -2,10 +2,13 @@ package com.ralphmarondev.cowntdown.core.worker
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.ralphmarondev.cowntdown.MainActivity
 import java.time.LocalDate
 
 class FirstDayOfMonthWorker(
@@ -42,14 +45,27 @@ class FirstDayOfMonthWorker(
     }
 
     private fun sendNotification() {
-        val notificationManager =
-            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
         val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
             .setContentTitle("Cowntdown")
             .setContentText("Wake up, it's the first of the month!")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .build()
+
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         notificationManager.notify(NOTIFICATION_ID, notification)
     }
