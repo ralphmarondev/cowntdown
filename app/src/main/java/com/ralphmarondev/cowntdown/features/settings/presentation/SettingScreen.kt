a405f54015cfc5a05a2ae9572da9e072ecc9bb08
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-02-01 15:14:09 +0800

    updated app icon

--- app/src/main/java/com/ralphmarondev/cowntdown/features/settings/presentation/SettingScreen.kt
+++ app/src/main/java/com/ralphmarondev/cowntdown/features/settings/presentation/SettingScreen.kt
@@ -1,8 +1,13 @@
 package com.ralphmarondev.cowntdown.features.settings.presentation
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -12,11 +17,15 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.ralphmarondev.cowntdown.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,20 +46,40 @@ fun SettingScreen() {
             )
         }
     ) { innerPadding ->
-        Box(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Settings",
-                fontWeight = FontWeight.W500,
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.secondary,
-                modifier = Modifier.padding(16.dp)
-            )
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    painter = rememberAsyncImagePainter(R.drawable.app_icon),
+                    contentDescription = "App icon",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(140.dp)
+                        .clip(CircleShape)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Cowntdown",
+                    fontWeight = FontWeight.W500,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.secondary
+                )
+
+                Text(
+                    text = "Version 1.0",
+                    fontWeight = FontWeight.W500,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+            }
         }
     }
 }
